b565e6e617eceadf5fac03dfd4f63a96045360f7
duanmh <menghuaduan@126.com>
2016-03-31 22:48:05 +0800

    去除警告信息

--- src/test/java/org/springframework/samples/app/aop/AopLogTest.java
+++ src/test/java/org/springframework/samples/app/aop/AopLogTest.java
@@ -1,10 +1,7 @@
 package org.springframework.samples.app.aop;
 
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-
 import javax.annotation.Resource;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +10,6 @@ import org.springframework.samples.app.service.IUserService;
 import org.springframework.samples.mvc.AbstractContextControllerTests;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
